45ab3ddce2456a939e52eb48edfb3012fd640c60
vidhishaprabhu <vidhishaprabhu@gmail.com>
2025-04-23 18:25:41 +0530

    Added the register functionality for AuthController file

--- crm-backend/app/Http/Controllers/AuthController.php
+++ crm-backend/app/Http/Controllers/AuthController.php
@@ -6,7 +6,20 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    public function register(){
-        
+    public function register(Request $request){
+        $request->validate([
+            'name'=>'required|string',
+            'email'=>'required|email|unique:users',
+            'password'=>'required|min:6|confirmed'
+        ]);
+        $user=User::create([
+            'name'=>$request->name,
+            'email'=>$request->email,
+            'password'=>$request->password,
+        ]);
+        return response()->json([
+            'message'=>'User registered successfully'
+
+        ]);
     }
 }
