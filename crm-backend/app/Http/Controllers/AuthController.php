0b0f14181ee87dd171b99bc4d9f2b408dcb19489
vidhishaprabhu <vidhishaprabhu@gmail.com>
2025-04-23 18:57:32 +0530

    Added the function for login,to get user information,logout in AuthController file

--- crm-backend/app/Http/Controllers/AuthController.php
+++ crm-backend/app/Http/Controllers/AuthController.php
@@ -15,11 +15,46 @@ class AuthController extends Controller
         $user=User::create([
             'name'=>$request->name,
             'email'=>$request->email,
-            'password'=>$request->password,
+            'password'=>bcrypt($request->password),
         ]);
         return response()->json([
             'message'=>'User registered successfully'
 
+        ]);
+    }
+    public function login(Request $request){
+        $request->validate([
+            'email'=>'required',
+            'password'=>'required'
+        ]);
+        $user=User::where('email',$request->email)->first();
+        if(!$user || Hash::make($request->password,$user->password)){
+            return ValidationException::withMessages([
+                'email'=>['Invalid Credentials']
+
+            ]);
+        }
+        return response()->json([
+            'message'=>'User logged in successfully',
+            'token'=>$user->createToken('auth_token')->plainTextToken,
+            'user'=>$user,
+            'role'=>$user->getRoleNames(),
+            'permission'=>$user->getPermissionNames()
+        ]);
+
+    }
+    public function user(Request $request){
+        $user=$request->user();
+        return response()->json([
+                'user'=>$user,
+                'roles'=>$user->getRoleNames(),
+                'permissions'=>$user->getPermissionNames()
+        ]);
+    }
+    public function logout(Request $request){
+        $request->user()->delete();
+        return response()->json([
+            'message'=>'User logged out successfully'
         ]);
     }
 }
