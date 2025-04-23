79280e130e5b7e7319e96dff02f2486e397c5ebc
vidhishaprabhu <vidhishaprabhu@gmail.com>
2025-04-23 20:24:57 +0530

    Added api route for user,index,store,destroy in api.php file

--- crm-backend/routes/api.php
+++ crm-backend/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,6 +19,10 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::middleware('auth:sanctum')->group(function(){
-    Route::get('/user',[AuthController::class,'index']);
+    Route::get('/user',[AuthController::class,'user']);
+    Route::get('/contacts',[ContactController::class,'index']);
+    Route::post('/contacts',[ContactController::class,'store'])->middleware('permission:create contact');
+    Route::delete('/contacts/{id}',[ContactController::class,'destroy'])->middleware('permission:delete contact');
+
 
 });
